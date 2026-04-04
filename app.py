fbdb76add99896aff7143c5a94d06375f12c3e47
Navdeep Singh <navdeep24816@gmail.com>
2026-04-04 21:11:07 +0530

    removed some error

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def load_encodings():
         data = pickle.load(f)
     return data["encodings"], data["names"]
 
-# ---------- Recognition ----------
+# Recognition
 def recognize_faces(frame, known_encodings, known_names, tolerance=0.50):
     rgb   = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     small = cv2.resize(rgb, (0, 0), fx=0.5, fy=0.5)

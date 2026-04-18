7d838282decb7d1ad6714aad68c47f87a1cfa133
Navdeep Singh <navdeep24816@gmail.com>
2026-04-18 22:53:42 +0530

    added comments for better understanding

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ init_db()
 st.title("🎓 Face Recognition Attendance System")
 st.markdown("---")
 
-
 col1, col2, col3, col4 = st.columns(4)
 col1.page_link("pages/1_📸_Register.py",    label="📸 Register Student",  icon="➕")
 col2.page_link("pages/2_📷_Attendance.py",  label="📷 Take Attendance",   icon="✅")
